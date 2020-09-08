441be9f5b58cdf076aeab03d60357f174fe38ba1
ArthurFDLR <arthfind@gmail.com>
2020-09-08 09:24:25 -0500

    Pip package structure

---
+++ beancount-ce/__init__.py
@@ -0,0 +1,1 @@
+print('Hey, I am the CE importer.')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='beancount-ce',
+    version='0.0.1',
+    description="Beancount importer for PDF statements from the French bank Caisse d'Epargne",
+    url='git@github.com:ArthurFDLR/beancount-ce.git',
+    author='Arthur Findelair',
+    author_email='arthfind@gmail.com',
+    license='MIT',
+    packages=['beancount-ce'],
+    zip_safe=False
+)

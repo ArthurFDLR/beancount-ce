3b4ed676e7b83e37e157bee62a3946b0fb9cca53
ArthurFDLR <arthfind@gmail.com>
2020-09-08 22:56:59 -0500

    Update Markdown

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# beancount-ce
+# Beancount Caisse d'Epargne Importer
+
+![GitHub](https://img.shields.io/github/license/ArthurFDLR/beancount-ce)
+
+
+`beancount-ce` provides a PDF statements importer for the bank [Caisse d'Epargne](http://www.caisse-epargne.fr) to the [Beancount](http://furius.ca/beancount/) format.
+
+## Installation
+
+```console
+    $ pip install beancount-ce
+```
+
+## Usage
+
+```python
+    CONFIG = [
+        CEImporter(
+            accountNumber=ACCOUNT_NUMBER,
+            account='Assets:FR:CdE:CompteCourant',
+            expenseCat='Expenses:FIXME',
+            creditCat='Income:FIXME',
+            showOperationTypes=False
+        ),
+    ]
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import os
 from setuptools import setup
+
+with open(
+        os.path.join(os.path.dirname(__file__), 'README.md'),
+        'r',
+        newline='\n',
+        encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='beancount-ce',
-    version='0.0.1',
+    version='0.1',
     description="Beancount importer for PDF statements from the French bank Caisse d'Epargne",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='git@github.com:ArthurFDLR/beancount-ce.git',
     author='Arthur Findelair',
     author_email='arthfind@gmail.com',
@@ -11,7 +21,7 @@ setup(
     packages=['beancount_ce'],
     zip_safe=False,
     install_requires=[
-        #'beancount>=2.1.3',
+        #'beancount>=2.1.3', #Cause error in venv installation
         'regex'
     ]
 )
